86f044e847461799c1260de1cf625b6f69f36797
Osemu <vincentaimiosior@gmail.com>
2024-09-28 03:24:15 +0100

    updates

--- contracts/yieldFarm.sol
+++ contracts/yieldFarm.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract yieldFarm is ERC20{
+contract yieldFarm is ERC20 {
     address public immutable owner ;
 
     constructor() ERC20("Holi", "HL") {
@@ -29,10 +29,15 @@ contract yieldFarm is ERC20{
         uint _amount;
     }
 
-    uint Id = 0;
+    uint Id = 1;
 
     mapping(uint => poolDetails) listOfPools;
     mapping(uint => mapping(address => userDeposit)) depositLogs;
+    mapping(address => mapping(uint => userDeposit)) userPoolDepositInfo;
+    mapping(address => uint[]) public usersPool;
+    mapping(uint => address[]) public arrayOfPoolDepositors;
+    mapping(address => uint) public userTotalDeposit;
+    mapping(address => uint) userTotalClaims;
 
     function addPool(uint maxAmount, uint yieldPercent, uint minDeposit, uint rewardTime) public onlyOwner {
         poolDetails memory newPool = poolDetails(
@@ -42,19 +47,24 @@ contract yieldFarm is ERC20{
             rewardTime,
             0
         );
+
         listOfPools[Id] = newPool;
         Id += 1;
     }
 
-    function depositWei(uint poolId) public payable {
+    function depositWei(uint poolId, uint amount) public payable {
         poolDetails memory depositingPool = listOfPools[poolId];
 
-        require(msg.value >= depositingPool._minDeposit, "Add more wei to depositing amount");
+        require(amount >= depositingPool._minDeposit, "Add more wei to depositing amount");
         require((poolId <= Id && poolId >= 0), "Invalid Pool");
         require(depositLogs[poolId][msg.sender]._poolId != poolId , "Unathorized");
 
-        depositingPool._currentAmount += msg.value;
-        depositLogs[poolId][msg.sender] = userDeposit(poolId, block.timestamp, msg.value);
+        arrayOfPoolDepositors[poolId].push(msg.sender);
+        listOfPools[poolId]._currentAmount += amount;
+        depositLogs[poolId][msg.sender] = userDeposit(poolId, block.timestamp, amount);
+        
+        userTotalDeposit[msg.sender] += amount;
+        usersPool[msg.sender].push(poolId);
     }
 
     function withdrawWei(uint poolId, uint amount) public payable{
@@ -72,7 +82,7 @@ contract yieldFarm is ERC20{
         uint depositDate = depositLogs[poolId][msg.sender].timestamp;
         uint daysCount = listOfPools[poolId]._rewardTime;
 
-        uint timePeriodInDays = (depositDate - block.timestamp)/secondsInDay;
+        uint timePeriodInDays = (block.timestamp - depositDate)/secondsInDay;
         require(timePeriodInDays > 0, "Invalid trasaction");
 
         uint yieldClaims = (timePeriodInDays % daysCount) * listOfPools[poolId]._yieldPercent;
@@ -83,8 +93,51 @@ contract yieldFarm is ERC20{
     }
 
     function checkPoolDetails(uint poolId) public view returns (uint, uint, uint, uint) {
+        require(poolId < Id && poolId > 0, "Invalid Pool");
         poolDetails memory result = listOfPools[poolId];
+        
         return (result._maxAmount, result._yieldPercent, result._minDeposit, result._rewardTime);
     }
-}
 
+    function checkUserDeposits(address user) public view returns (uint, uint) {
+        uint[] memory userDepositingPools = usersPool[user];
+        uint deposit = userTotalDeposit[user];
+        uint claims = 0;
+
+        if(deposit == 0){
+            return (deposit, 0);
+        } else {
+            for(uint i = 0; i < userDepositingPools.length; i++){
+                 uint256 secondsInDay = 86400;
+                 uint depositDate = depositLogs[userDepositingPools[i]][user].timestamp;
+                 uint daysCount = listOfPools[userDepositingPools[i]]._rewardTime;
+
+                 uint timePeriodInDays = (block.timestamp - depositDate)/secondsInDay;
+
+                 uint yieldClaims = (timePeriodInDays % daysCount) * listOfPools[userDepositingPools[i]]._yieldPercent;
+                 claims += yieldClaims * depositLogs[userDepositingPools[i]][user]._amount;
+            }
+
+            return (deposit, claims);
+        }
+    }
+
+    function checkUserDepositInPool(uint poolId) public view returns (address[] memory, uint[] memory) {
+        address[] memory depositors = arrayOfPoolDepositors[poolId];
+        uint[] memory amounts = new uint[](depositors.length);
+
+        for(uint i =0; i < depositors.length; i++){
+            uint deposit = depositLogs[poolId][depositors[i]]._amount;
+            amounts[i] = deposit;
+        }
+
+        return (depositors, amounts);
+    }
+
+    function checkClaimableRewards(uint poolId) public view returns (uint) {}
+
+    function checkRemainingCapacity(uint poolId) public view returns (uint) {}
+
+    function checkWhaleWallets() public view returns (address[] memory) {}
+
+}
--- test/yieldFarmTest.js
+++ test/yieldFarmTest.js
@@ -13,12 +13,16 @@ describe("YieldFarm Contract", function(){
         myContract.addPool(1000,3,100,600);
     })
 
-    it("Should give the right result", async function (){
+    it("Should give the right pool details", async function (){
             const poolDetails = await myContract.checkPoolDetails(0);
             expect(poolDetails[0]).to.equal(1000);  // first value
             expect(poolDetails[1]).to.equal(3);     // second value
             expect(poolDetails[2]).to.equal(100);   // third value
             expect(poolDetails[3]).to.equal(600);   // fourth value*/
         });
+    
+    it("User should be able to deposit wei", function (){
+        
+    })
    
 })
